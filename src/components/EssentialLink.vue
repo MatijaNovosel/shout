0003a4d0036fd2a1fb9cbcb1110eb7a01bf96b1a
Matija Novosel <mnovosel5@gmail.com>
2021-10-15 09:57:10 +0200

    Boilerplate files changes, formatting config

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,19 +1,12 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
+  <q-item clickable tag="a" target="_blank" :href="link">
+    <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
-
     <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label>
+        {{ title }}
+      </q-item-label>
       <q-item-label caption>
         {{ caption }}
       </q-item-label>
@@ -22,30 +15,27 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'EssentialLink',
+  name: "EssentialLink",
   props: {
     title: {
       type: String,
       required: true
     },
-
     caption: {
       type: String,
-      default: ''
+      default: ""
     },
-
     link: {
       type: String,
-      default: '#'
+      default: "#"
     },
-
     icon: {
       type: String,
-      default: ''
+      default: ""
     }
   }
-})
+});
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,10 @@
-import { store } from 'quasar/wrappers'
-import { createStore } from 'vuex'
+import { store } from "quasar/wrappers";
+import { createStore } from "vuex";
 
-// import example from './module-example'
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
-export default store(function (/* { ssrContext } */) {
+export default store(function () {
   const Store = createStore({
-    modules: {
-      // example
-    },
-
-    // enable strict mode (adds overhead!)
-    // for dev mode and --debug builds only
+    modules: {},
     strict: process.env.DEBUGGING
-  })
-
-  return Store
-})
+  });
+  return Store;
+});
